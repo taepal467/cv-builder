dbb8367e9906bb23adb72e7655826eb145990a17
Chantae Palacio <chantae.palacio@gmail.com>
2022-12-18 17:52:12 -0800

    Updates

---
+++ precache-manifest.dabfd0a98f1d213644a58c61a46ce5c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f8617f42098b27e6253",
+    "url": "/cv-project/static/css/main.2492c62e.chunk.css"
+  },
+  {
+    "revision": "3f8617f42098b27e6253",
+    "url": "/cv-project/static/js/main.3f8617f4.chunk.js"
+  },
+  {
+    "revision": "00671ee0d75cbe278a38",
+    "url": "/cv-project/static/js/1.00671ee0.chunk.js"
+  },
+  {
+    "revision": "191a181b6a79c31ed82c",
+    "url": "/cv-project/static/js/runtime~main.191a181b.js"
+  },
+  {
+    "revision": "a0d0f724878dc672bb032e689b2319ab",
+    "url": "/cv-project/index.html"
+  }
+];
